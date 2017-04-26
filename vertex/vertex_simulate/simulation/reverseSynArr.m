5c1854f1ed22896976d1cccb10d02893da9f22a9
b3046588 <b3046588@localhost>
2017-04-26 15:40:01 +0100

    improved efficiency of reverseSynArr

---
+++ vertex/vertex_simulate/simulation/reverseSynArr.m
@@ -0,0 +1,19 @@
+function [ revsynarr ] =reverseSynArr( synarr )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+numrows = length(synarr);
+revsynarr = cell(numrows,1);
+count = ones(numrows,1);
+
+for i= 1:numrows
+    for j = 1:length(synarr{i,1})
+        
+        revsynarr{synarr{i,1}(j),1}(count(synarr{i,1}(j))) = i;
+        revsynarr{synarr{i,1}(j),2}(count(synarr{i,1}(j))) = j;
+        count(synarr{i,1}(j)) = count(synarr{i,1}(j)) + 1;
+    end
+    
+end
+
+end
+
