e23eb337067daf91c7d0ff353e2deb216ed58283
b3046588 <b3046588@cypher238.ncl.ac.uk>
2018-11-29 09:21:37 +0000

    renamed the electrode file

--- ratSomatosensoryCortex/setUpBipolarElectrodeStimulation.m
+++ ratSomatosensoryCortex/setUpBipolarElectrodeStimulation.m
@@ -1,3 +1,3 @@
-[TissueParams.StimulationField,model] = invitroSliceStim('6layermodelstiml4placedinsmall.stl',-750); % slicecutoutsmallnew
+[TissueParams.StimulationField,model] = invitroSliceStim('6layermodelstiml4placedin.stl',-750); % slicecutoutsmallnew
 TissueParams.StimulationOn = [1500]; % Turn stimulation on at 1500 ms
 TissueParams.StimulationOff = [1501]; % Turn stimulation off at 1501 ms
