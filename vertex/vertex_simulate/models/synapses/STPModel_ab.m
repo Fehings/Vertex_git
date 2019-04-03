9f3bd36287c5a121ef89ed18c3ee9d6b9bc1256b
b3046588 <b3046588@cypher238.ncl.ac.uk>
2019-04-03 14:06:33 +0100

    Fixed bug in AB synapse model

--- vertex/vertex_simulate/models/synapses/STPModel_ab.m
+++ vertex/vertex_simulate/models/synapses/STPModel_ab.m
@@ -70,8 +70,9 @@ classdef (Abstract) STPModel_ab < STPModel
 
     
     
-    function [F, D] = getSTPVars(SM, preInd, groupID)
+    function [VAR] = getSTPVars(SM, preInd, groupID)
         preInd = preInd + SM.preBoundaryArr(SM.preGroupIDs==groupID);
+        VAR = [SM.F(preInd), SM.D(preInd), zeros(length(preInd),1), zeros(length(preInd),1)];
         F = SM.F(preInd);
         D = SM.D(preInd);
     end
